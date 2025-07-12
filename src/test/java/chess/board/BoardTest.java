63391caa35f8447b0864713f40f28fce87179240
Nicolas de Fisser <nic.defisser@gmail.com>
2025-07-12 14:23:03 +0200

    fix: piece.equals overrides

--- src/test/java/chess/board/BoardTest.java
+++ src/test/java/chess/board/BoardTest.java
@@ -1,39 +1,39 @@
-//package chess.board;
-//
-//import chess.board.pieces.*;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class BoardTest {
-//
-//    @Test
-//    public void fillBoardEmpty() {
-//        int boardSize = 8;
-//        Board board = new Board(boardSize);
-//        Piece[][] actualGrid = board.fillBoard(new EmptyPiece());
-//
-//        assertAll(() -> {
-//            for (int row = 0; row < boardSize; row++) {
-//                for (int collumn = 0; collumn < boardSize; collumn++) {
-//                    assertEquals(new EmptyPiece(), actualGrid[row][collumn]);
-//                }
-//            }
-//        });
-//    }
-//
-//    @Test
-//    public void fillBoardAllKings() {
-//        int boardSize = 8;
-//        Board board = new Board(boardSize);
-//        Piece[][] actualGrid = board.fillBoard(new King());
-//
-//        assertAll(() -> {
-//            for (int row = 0; row < boardSize; row++) {
-//                for (int collumn = 0; collumn < boardSize; collumn++) {
-//                    assertEquals(new King(), actualGrid[row][collumn]);
-//                }
-//            }
-//        });
-//    }
-//}
+package chess.board;
+
+import chess.board.pieces.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardTest {
+
+    @Test
+    public void fillBoardEmpty() {
+        int boardSize = 8;
+        Board board = new Board(boardSize);
+        Piece[][] actualGrid = board.fillBoard(new EmptyPiece());
+
+        assertAll(() -> {
+            for (int row = 0; row < boardSize; row++) {
+                for (int collumn = 0; collumn < boardSize; collumn++) {
+                    assertEquals(new EmptyPiece(), actualGrid[row][collumn]);
+                }
+            }
+        });
+    }
+
+    @Test
+    public void fillBoardAllKings() {
+        int boardSize = 8;
+        Board board = new Board(boardSize);
+        Piece[][] actualGrid = board.fillBoard(new King());
+
+        assertAll(() -> {
+            for (int row = 0; row < boardSize; row++) {
+                for (int collumn = 0; collumn < boardSize; collumn++) {
+                    assertEquals(new King(), actualGrid[row][collumn]);
+                }
+            }
+        });
+    }
+}
