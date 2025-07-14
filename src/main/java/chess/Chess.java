114f91d6043c6c05bd9f2164c76ef17a9f46f89e
Nicolas de Fisser <nic.defisser@gmail.com>
2025-07-14 16:10:34 +0200

    feat: added boardDisplay.java

--- src/main/java/chess/Chess.java
+++ src/main/java/chess/Chess.java
@@ -1,20 +1,24 @@
 package chess;
 
 import chess.board.Board;
+import chess.display.Display;
 import chess.json.*;
 
 public class Chess {
     private static int SIZE = 8;
     Board board;
     BoardExtractor boardExtractor;
+    Display display;
 
     public Chess(BoardExtractor bExtractor) {
         board = new Board(SIZE);
         boardExtractor = bExtractor;
         board.getGridFromSave(bExtractor);
+//        display = new Display();
     }
 
     public void play() {
         System.out.println(board.getBoardString());
+        while (true);
     }
 }
