c9e26c916c1dbf9be7797daac8ed2bb9f5a8b8f2
Nicolas de Fisser <nic.defisser@gmail.com>
2025-07-16 00:57:46 +0200

    fix: fixed pawn moving diagonally

--- src/main/java/chess/board/pieces/Pawn.java
+++ src/main/java/chess/board/pieces/Pawn.java
@@ -19,19 +19,52 @@ public class Pawn extends Piece {
                 (!isWhite() && position.y == 6);
     }
 
+//    private void addDiagonalMoves(List<Point> list, Point position, Piece[][] boardGrid) {
+//        int multiplier = (isWhite) ? 1 : -1;
+//        Point tempPoint = new Point(position.x + 1, position.y + multiplier);
+//        if (boardGrid[tempPoint.y][tempPoint.x].isWhite != this.isWhite) list.add(tempPoint);
+//        tempPoint = new Point(position.x - 1, position.y + multiplier);
+//        if (boardGrid[tempPoint.y][tempPoint.x].isWhite != this.isWhite) list.add(tempPoint);
+//    }
+
     @Override
     public Point[] getMoveablePoints(Point position, Piece[][] boardGrid) {
         List<Point> outputArray = new ArrayList<Point>();
         int multiplier = (isWhite) ? 1 : -1;
+        int boardSize = boardGrid.length;
 
-        Point tempPoint = new Point(position.x,
+
+        Point tempPoint = new Point(position.x + 1, position.y + multiplier);
+        if (tempPoint.x >= 0 && tempPoint.x < boardSize
+                && tempPoint.y >= 0 && tempPoint.y < boardSize) {
+
+            Piece targetPiece = boardGrid[tempPoint.y][tempPoint.x];
+            if (targetPiece.getType() != PieceType.EMPTY &&
+                    targetPiece.isWhite != this.isWhite) outputArray.add(tempPoint);
+        }
+
+        tempPoint = new Point(position.x - 1, position.y + multiplier);
+        if (tempPoint.x >= 0 && tempPoint.x < boardSize
+                && tempPoint.y >= 0 && tempPoint.y < boardSize) {
+
+            Piece targetPiece = boardGrid[tempPoint.y][tempPoint.x];
+            if (targetPiece.getType() != PieceType.EMPTY &&
+                    targetPiece.isWhite != this.isWhite) outputArray.add(tempPoint);
+        }
+
+         tempPoint = new Point(position.x,
                 position.y + multiplier);
-        if (tempPoint.y < 0 || tempPoint.y > boardGrid.length) return outputArray.toArray(new Point[0]);
+
+        if (tempPoint.y < 0 || tempPoint.y >= boardGrid.length ||
+                boardGrid[tempPoint.y][tempPoint.x].getType() != PieceType.EMPTY)
+            return outputArray.toArray(new Point[0]);
+
         outputArray.add(tempPoint);
 
         if (!isOnStartingPosition(position)) return outputArray.toArray(new Point[0]);
         tempPoint = new Point(position.x, position.y + (2 * multiplier));
         outputArray.add(tempPoint);
+
         return outputArray.toArray(new Point[0]);
     }
 }
