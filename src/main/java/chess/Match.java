f6d09a7c093197a7e70b7840082c89a0029c2f05
Nicolas de Fisser <nic.defisser@gmail.com>
2025-07-16 19:06:34 +0200

    added white/black turn by turn rule

--- src/main/java/chess/Match.java
+++ src/main/java/chess/Match.java
@@ -2,18 +2,26 @@ package chess;
 
 import chess.board.*;
 import chess.board.pieces.Piece;
+import chess.board.pieces.PieceType;
 
-import java.awt.*;
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Match {
     private static int SIZE = 8;
 
     private BoardState boardState;
-    private BoardState[] stateList;
+    private List<BoardState> stateList;
+
+    private Boolean whiteTurn;
 
     public Match(Piece[][] boardGrid) {
         boardState = new BoardState(SIZE);
         boardState.setGrid(boardGrid);
+        whiteTurn = true;
+        stateList = new ArrayList<BoardState>();
+        stateList.add(boardState);
     }
 
     public BoardState getBoardState() {
@@ -25,6 +33,30 @@ public class Match {
     }
 
     public void handleSelectSquare(Point square) {
-        boardState.handleSelected(square);
+        Piece clickedPiece = boardState.getPiece(square);
+        Piece selectedPiece = boardState.getSelectedPiece();
+
+        if (selectedPiece == null) {
+            if (clickedPiece == null || clickedPiece.isWhite() != whiteTurn) {
+                return;
+            }
+        }
+
+        if (clickedPiece.getType() == PieceType.EMPTY && !boardState.getMoveablePoints().contains(square)) return;
+
+        if (selectedPiece != null) {
+            boolean isReselectingOwnPiece = clickedPiece != null && clickedPiece.isWhite() == whiteTurn;
+            boolean isMoveAttempt = boardState.getMoveablePoints().contains(square);
+
+            if (!isReselectingOwnPiece && !isMoveAttempt) {
+                return;
+            }
+        }
+
+        boolean didMove = boardState.handleSelected(square);
+        if (didMove) {
+            whiteTurn = !whiteTurn;
+            stateList.add(boardState); // maybe make a deep copy here
+        }
     }
 }
